c6d78b5c8778621470e27d6927a48702490b021c
whitehatjr <whitehatjr@whitehatjr.com>
2020-09-25 08:48:42 +0530

    scraper with output

--- scrapper_2.py
+++ scrapper_2.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import csv
-
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
 browser = webdriver.Chrome("./chromedriver")
 browser.get(START_URL)
@@ -11,10 +10,8 @@ time.sleep(10)
 headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude", "discovery_date", "hyperlink", "planet_type", "planet_radius", "orbital_radius", "orbital_period", "eccentricity"]
 planet_data = []
 new_planet_data = []
-
 def scrape():
     for i in range(0, 428):
-        time.sleep(2)
         soup = BeautifulSoup(browser.page_source, "html.parser")
         for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
             li_tags = ul_tag.find_all("li")
@@ -31,30 +28,29 @@ def scrape():
             temp_list.append("https://exoplanets.nasa.gov"+hyperlink_li_tag.find_all("a", href=True)[0]["href"])
             planet_data.append(temp_list)
         browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-
+        print(f"{i+1} page done 1")
 def scrape_more_data(hyperlink):
-    time.sleep(2)
     page = requests.get(hyperlink)
     soup = BeautifulSoup(page.content, "html.parser")
+    temp_list = []
     for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
         td_tags = tr_tag.find_all("td")
-        temp_list = []
         for td_tag in td_tags:
             try:
                 temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
             except:
                 temp_list.append("")
-        new_planet_data.append(temp_list)
-
+    new_planet_data.append(temp_list)
 scrape()
-for data in planet_data:
-    scrape_more_data(data[5])
-
-final_planet_data = []
-
 for index, data in enumerate(planet_data):
-    final_planet_data.append(data + final_planet_data[index])
-
+    scrape_more_data(data[5])
+    print(f"{index+1} page done 2")
+final_planet_data = []
+for index, data in enumerate(planet_data):
+    new_planet_data_element = new_planet_data[index]
+    new_planet_data_element = [elem.replace("\n", "") for elem in new_planet_data_element]
+    new_planet_data_element = new_planet_data_element[:7]
+    final_planet_data.append(data + new_planet_data_element)
 with open("final.csv", "w") as f:
         csvwriter = csv.writer(f)
         csvwriter.writerow(headers)
