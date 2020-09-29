f680e6cad402008e563db60d5d642f98412d9906
whitehatjr <whitehatjr@whitehatjr.com>
2020-09-29 06:20:01 +0530

    final csv

--- scrapper_2.py
+++ scrapper_2.py
@@ -11,8 +11,17 @@ headers = ["name", "light_years_from_earth", "planet_mass", "stellar_magnitude",
 planet_data = []
 new_planet_data = []
 def scrape():
-    for i in range(0, 428):
-        soup = BeautifulSoup(browser.page_source, "html.parser")
+    for i in range(1, 430):
+        while True:
+            time.sleep(2)
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+            current_page_num = int(soup.find_all("input", attrs={"class", "page_num"})[0].get("value"))
+            if current_page_num < i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
+            elif current_page_num > i:
+                browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[1]/a').click()
+            else:
+                break
         for ul_tag in soup.find_all("ul", attrs={"class", "exoplanet"}):
             li_tags = ul_tag.find_all("li")
             temp_list = []
@@ -28,19 +37,23 @@ def scrape():
             temp_list.append("https://exoplanets.nasa.gov"+hyperlink_li_tag.find_all("a", href=True)[0]["href"])
             planet_data.append(temp_list)
         browser.find_element_by_xpath('//*[@id="primary_column"]/footer/div/div/div/nav/span[2]/a').click()
-        print(f"{i+1} page done 1")
+        print(f"{i} page done 1")
 def scrape_more_data(hyperlink):
-    page = requests.get(hyperlink)
-    soup = BeautifulSoup(page.content, "html.parser")
-    temp_list = []
-    for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
-        td_tags = tr_tag.find_all("td")
-        for td_tag in td_tags:
-            try:
-                temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
-            except:
-                temp_list.append("")
-    new_planet_data.append(temp_list)
+    try:
+        page = requests.get(hyperlink)
+        soup = BeautifulSoup(page.content, "html.parser")
+        temp_list = []
+        for tr_tag in soup.find_all("tr", attrs={"class": "fact_row"}):
+            td_tags = tr_tag.find_all("td")
+            for td_tag in td_tags:
+                try:
+                    temp_list.append(td_tag.find_all("div", attrs={"class": "value"})[0].contents[0])
+                except:
+                    temp_list.append("")
+        new_planet_data.append(temp_list)
+    except:
+        time.sleep(1)
+        scrape_more_data(hyperlink)
 scrape()
 for index, data in enumerate(planet_data):
     scrape_more_data(data[5])
